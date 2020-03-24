04c717e746ff093e67648dab0c69d4938f4863fa
tippitytapp <60143534+tippitytapp@users.noreply.github.com>
2020-03-24 17:25:09 -0400

    finished mvp

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { v4 as uuid } from 'uuid';
 import logo from './logo.svg';
+import Form from "./component/form"
 import './App.css';
 
+const initialTeam =[
+  {id: uuid(), fname: 'Marc Tapp', email:'marc.tapp@me.com', role:'WebDev' }
+];
+
 function App() {
+
+  const [team, setTeam] = useState(initialTeam)
+  const [teamInfo, setTeamInfo] = useState({fname:"", email:"", role:""})
+
+  const onInputChange = e => {
+    const inputChange = e.target.name
+    const newValue = e.target.value
+    setTeamInfo({
+      ...teamInfo, 
+      [inputChange]:newValue,
+    })
+  }
+
+  const onFSubmit = e => {
+    e.preventDefault()
+    const newTeamMember = {
+      id: uuid(),
+      fname: teamInfo.fname,
+      email: teamInfo.email,
+      role: teamInfo.role,
+    }
+    setTeam([...team, newTeamMember])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+        <Form 
+        onInputChange={onInputChange}
+        teamInfo={teamInfo}
+        onFSubmit={onFSubmit}
+         />
+        <h3>List of Team Members</h3>
+        {
+          team.map(f => <div key={f.id}>Name: {f.fname}    Role:{f.role}    Email: {f.email}</div>)
+        }
+
+     
+
     </div>
   );
 }
---
+++ team-builder/src/component/form.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+function Form(props){
+    
+return(
+    <form onSubmit={props.onFSubmit}>
+        <label>Name: 
+            <input
+            onChange={props.onInputChange}
+            value={props.teamInfo.fname}
+            name="fname"
+            type="text"
+            />
+        </label><br />
+        <label>Email:
+            <input
+            onChange={props.onInputChange}
+            value={props.teamInfo.email}
+            name="email"
+            type="email"
+            />
+        </label><br />
+        <label>Role:
+            <input
+            onChange={props.onInputChange}
+            value={props.teamInfo.role}
+            name="role"
+            type="text"
+            />
+        </label><br />
+            <button>Submit</button>
+            <button>Edit</button> 
+          
+    </form>
+)
+}
+
+
+export default Form;
